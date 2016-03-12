597bf900728e5d634c75fb1702a14a32e49546a3
notsliva <notsliva@ya.ru>
2016-03-13 02:21:10 +0600

    dialpad mark-up 001

--- platforms/android/assets/www/js/trim.js
+++ platforms/android/assets/www/js/trim.js
@@ -1,4 +1,4 @@
-// trim('Hello World', 'Hdle'); = 'o Wor'
+// trim('Hello World', 'Hdle') == 'o Wor'
 function trim(str, charlist) {
 	charlist = !charlist ? ' \s\xA0' : charlist.replace(/([\[\]\(\)\.\?\/\*\{\}\+\$\^\:])/g, '\$1');
 	//var re = new RegExp('^[' + charlist + ']+|[' + charlist + ']+$', 'g');
